c08b04c592e062c1d84e77c6d0fe285982c73924
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-04-30 20:44:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
+import os
 # Токен бота от @BotFather
-BOT_TOKEN = ""
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
 # ID администратора (парикмахера), узнать у @userinfobot
-ADMIN_ID = 0
+ADMIN_ID = int(os.getenv("ADMIN_ID",0))
 
 # Телефон мастера (для напоминаний и связи)
-MASTER_PHONE = "+7 (900) 123-45-67"
+MASTER_PHONE = os.getenv("MASTER_PHONE", "")
