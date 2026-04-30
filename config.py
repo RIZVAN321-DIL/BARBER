c0d707dcc7a461278804c6ec62570ac7e2cdec56
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-04-30 22:27:28 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-# Токен бота от @BotFather
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-
-# ID администратора (парикмахера), узнать у @userinfobot
-ADMIN_ID = int(os.getenv("ADMIN_ID",0))
-
-# Телефон мастера (для напоминаний и связи)
-MASTER_PHONE = os.getenv("MASTER_PHONE", "")
